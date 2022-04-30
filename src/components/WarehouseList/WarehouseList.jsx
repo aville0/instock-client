1ba8b3af1077952bb3996fa93cb014d2c012ddc3
brandodo <brandonong93@gmail.com>
2022-04-30 18:01:44 -0400

    tweaked warehouselist

--- src/components/WarehouseList/WarehouseList.jsx
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -63,75 +63,77 @@ class WarehouseList extends React.Component {
   render() {
     return (
       <>
-        {this.state.allWarehouses.map((warehouse) => {
-          return (
-            <div key={warehouse.id} className="warehouse__information">
-              <div className="warehouse__information-data">
-                <div className="warehouse__information-top">
-                  <div className="warehouse__information-location">
-                    <h4 className="warehouse__subheader">WAREHOUSE</h4>
-                    <Link
-                      to={`warehouses/${warehouse.id}`}
-                      className="warehouse__location"
-                    >
-                      <p>{warehouse.name}</p>
-                      <img
-                        className="warehouse__chevron"
-                        src={Chevron}
-                        alt="trashcan"
-                      />
-                    </Link>
+        {this.state.allWarehouses &&
+          this.state.allWarehouses.map((warehouse) => {
+            return (
+              <div key={warehouse.id} className="warehouse__information">
+                <div className="warehouse__information-data">
+                  <div className="warehouse__information-top">
+                    <div className="warehouse__information-location">
+                      <h4 className="warehouse__subheader">WAREHOUSE</h4>
+                      <Link
+                        to={`warehouses/${warehouse.id}`}
+                        className="warehouse__location"
+                      >
+                        <p>{warehouse.name}</p>
+                        <img
+                          className="warehouse__chevron"
+                          src={Chevron}
+                          alt="trashcan"
+                        />
+                      </Link>
+                    </div>
+                    <div className="warehouse__information-address">
+                      <h4 className="warehouse__subheader">ADDRESS</h4>
+                      <p className="warehouse__address-details">
+                        {warehouse.address}, {warehouse.city},{" "}
+                        {warehouse.country}
+                      </p>
+                    </div>
                   </div>
-                  <div className="warehouse__information-address">
-                    <h4 className="warehouse__subheader">ADDRESS</h4>
-                    <p className="warehouse__address-details">
-                      {warehouse.address}, {warehouse.city}, {warehouse.country}
-                    </p>
+                  <div className="warehouse__information-bottom">
+                    <div className="warehouse__information-contact">
+                      <h4 className="warehouse__subheader">CONTACT NAME</h4>
+                      <p className="warehouse__contact-name">{`${get(
+                        warehouse,
+                        "contact.name"
+                      )}`}</p>
+                    </div>
+                    <div className="warehouse__information-contact-information">
+                      <h4 className="warehouse__subheader">
+                        CONTACT INFORMATION
+                      </h4>
+                      <p className="warehouse__contact-number">{`${get(
+                        warehouse,
+                        "contact.phone"
+                      )}`}</p>
+                      <p className="warehouse__contact-email">{`${get(
+                        warehouse,
+                        "contact.email"
+                      )}`}</p>
+                    </div>
                   </div>
                 </div>
-                <div className="warehouse__information-bottom">
-                  <div className="warehouse__information-contact">
-                    <h4 className="warehouse__subheader">CONTACT NAME</h4>
-                    <p className="warehouse__contact-name">{`${get(
-                      warehouse,
-                      "contact.name"
-                    )}`}</p>
-                  </div>
-                  <div className="warehouse__information-contact-information">
-                    <h4 className="warehouse__subheader">
-                      CONTACT INFORMATION
-                    </h4>
-                    <p className="warehouse__contact-number">{`${get(
-                      warehouse,
-                      "contact.phone"
-                    )}`}</p>
-                    <p className="warehouse__contact-email">{`${get(
-                      warehouse,
-                      "contact.email"
-                    )}`}</p>
-                  </div>
-                </div>
-              </div>
-              <div className="warehouse__actions">
-                <img
-                  name={warehouse.name}
-                  id={warehouse.id}
-                  onClick={this.onTrashHandler}
-                  className="warehouse__actions-trash"
-                  src={Trash}
-                  alt="trashcan"
-                />
-                <Link to={`/edit/warehouses/${warehouse.id}`}>
+                <div className="warehouse__actions">
                   <img
-                    className="warehouse__actions-edit"
-                    src={Edit}
-                    alt="edit"
+                    name={warehouse.name}
+                    id={warehouse.id}
+                    onClick={this.onTrashHandler}
+                    className="warehouse__actions-trash"
+                    src={Trash}
+                    alt="trashcan"
                   />
-                </Link>
+                  <Link to={`/edit/warehouses/${warehouse.id}`}>
+                    <img
+                      className="warehouse__actions-edit"
+                      src={Edit}
+                      alt="edit"
+                    />
+                  </Link>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
         <DelModal
           show={this.state.show}
           onCloseHandler={this.onCloseHandler}
